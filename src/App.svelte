e8a781ce8cdc01f3b6c34f811dbaa22947dcbf5b
Eric Liu <ericyl.us@gmail.com>
2020-05-02 17:10:04 -0700

    feat: initial commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { BarChartSimple } from "@carbon/charts-svelte";
+</script>
+
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://unpkg.com/@carbon/charts@0.30.10/styles.min.css" />
+</svelte:head>
+
+<BarChartSimple
+  data={[{ group: 'Qty', value: 65000 }, { group: 'More', value: 29123 }, { group: 'Sold', value: 35213 }, { group: 'Restocking', value: 51213 }, { group: 'Misc', value: 16932 }]}
+  options={{ title: 'Simple bar (discrete)', axes: { left: { mapsTo: 'value' }, bottom: { mapsTo: 'group', scaleType: 'labels' } }, height: '400px' }} />
